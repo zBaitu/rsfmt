1d1dd705f42e537d0f0cdedf945d76ac2dd63ab5
baitu <zbaitu@gmail.com>
2022-01-22 17:01:12 +0800

    [fmt]

--- src/ast.rs
+++ src/ast.rs
@@ -1,6 +1,15 @@
-pub use rustc_ap_rustc_ast::{ast::*, ptr::P, token::{DelimToken, Token, TokenKind}, tokenstream::TokenStream,
-                             util::comments::{CommentStyle, Comment}
+pub use rustc_ap_rustc_ast::{
+    ast::*,
+    ptr::P,
+    token::{DelimToken, Token, TokenKind},
+    tokenstream::TokenStream,
+    util::comments::{Comment, CommentStyle}
 };
-pub use rustc_ap_rustc_span::{BytePos, Span, SpanSnippetError, SyntaxContext, source_map::Spanned,
-                              symbol::{Ident, Symbol, kw}};
-
+pub use rustc_ap_rustc_span::{
+    BytePos,
+    Span,
+    SpanSnippetError,
+    SyntaxContext,
+    source_map::Spanned,
+    symbol::{Ident, Symbol, kw}
+};
